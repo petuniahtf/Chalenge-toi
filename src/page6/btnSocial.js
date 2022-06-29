87217711fc45784e30f7e52b9c66295b093688c2
petuniahtf <priscilliaweber6@hotmail.com>
2022-06-29 13:50:34 +0200

    création de grid

--- src/page6/btnSocial.js
+++ src/page6/btnSocial.js
@@ -1,15 +1,19 @@
 import React from "react";
+import './btnSocial.css'
+import avatar from'../image/avatar-michelle.jpg'
+
 
 
 const SocialBtn = () =>{
+    const arrowBtn = <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13"><path fill="#6E8098" d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"/></svg>
     return(
-        <div>
-            <img src="../image/avatar-michelle.jpg" alt="??"/>
+        <div className="container__social">
+            <img src={avatar} alt="??"/>
+            <div className="text1">
             <h2>Michelle Appleton</h2>
             <p>28 juin 2020</p>
-            <button className="btn__social">
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13"><path fill="#6E8098" d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"/></svg>
-            </button>
+            </div>
+            <button className="btn__social"> {arrowBtn}</button>
         </div>
     )
 }
--- src/page6/page6.js
+++ src/page6/page6.js
@@ -2,13 +2,24 @@ import React from "react";
 import SocialBtn from "./btnSocial";
 import { Button } from "@mui/material";
 import './page6.css'
+import commode from '../image/drawers.jpg'
 
-const Page6 = () =>{
-    return(
+const Page6 = () => {
+    return (
         <div>
             <Button variant="contained" href="/">Home</Button>
-            
-            <SocialBtn/>
+            <div className="container">
+                <div className="container__type">
+                <img src={commode} width='350px' height='350px' alt="..." />
+                <div className="container__tupe">
+                    <h1>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
+                    <p>Ever been in a room and felt like something was missing? Perhaps it felt slighty bare and uninviting. I've got some simple tips to help you make any room feel complete</p>    
+                </div>
+                <div className="container__tape">
+                <SocialBtn />
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
